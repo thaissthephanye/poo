a0ae6911a8ec76587c359b52f0d3a4bfd41ec2b3
thaissthephanye <thaissthephanye@gmail.com>
2025-04-23 11:02:22 -0300

    feat: q5

--- exercicios01/Q5/Funcionario.java
+++ exercicios01/Q5/Funcionario.java
@@ -1,13 +1,13 @@
 public class Funcionario {
     private int codigo;
     private String nome;
-    private int CPF;
+    private String CPF;
     private String endereco;
-    private int telefone;
+    private String telefone;
     private int idade;
     private float salario;
 
-    public Funcionario(int codigo, String nome, int CPF, String endereco, int telefone, int idade, float salario){
+    public Funcionario(int codigo, String nome, String CPF, String endereco, String telefone, int idade, float salario){
         this.codigo = codigo;
         this.nome = nome;
         this.CPF = CPF;
@@ -23,13 +23,13 @@ public class Funcionario {
     public String getNome(){
         return nome;
     }
-    public int getCPF(){
+    public String getCPF(){
         return CPF;
     }
     public String getEndereco(){
         return endereco;
     }
-    public int getTelefone(){
+    public String getTelefone(){
         return telefone;
     }
     public int getIdade(){
@@ -42,10 +42,35 @@ public class Funcionario {
     public void setCodigo(int codigo){
         this.codigo = codigo;
     }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setCPF(String CPF){
+        this.CPF = CPF;
+    }
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public void setSalario(float salario){
+        this.salario = salario;
+    }
 
-    // public float calculaSalarioLiquido(){
-    //     float liquido = salario - ;
-    // }
+
+    public float calculaSalarioLiquido(){
+        return salario * 0.89f;
+    }
+
+    public String toString(){
+        return String.format("Código: %d / Nome: %s / CPF: %s%n"+
+        "Endereco: %s / Telefone: %s / Idade: %d%n"+
+        "Salario bruto: R$%.2f / Salario liquido: R$%.2f", codigo, nome, CPF, endereco, telefone, idade, salario, calculaSalarioLiquido());
+    }
 
     
 
---
+++ exercicios01/Q5/Principal.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    Funcionario funcionario = new Funcionario(1001, "Thais", "11122233344", "Rua pipipi popopo", "83991151519", 23, 1200.12f);
+
+    System.out.println("")
+
+}
