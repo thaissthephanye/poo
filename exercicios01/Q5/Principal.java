a0718242b1b0eff89cc872fc83651ed20965daee
thaissthephanye <thaissthephanye@gmail.com>
2025-04-23 21:04:38 -0300

    fix: scanner ajustado

--- exercicios01/Q5/Principal.java
+++ exercicios01/Q5/Principal.java
@@ -3,10 +3,26 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        Funcionario funcionario = new Funcionario(1001, "Thais", "11122233344", "Rua pipipi popopo", "83991151519", 23, 1200.12f);
+
+        System.out.println("---FUNCIONARIOS---");
+        System.out.println(funcionario);
+
+        System.out.println("Digite o valor do novo salario: ");
+
+        float novoSalario = scanner.nextFloat();
+        
+        funcionario.setSalario(novoSalario); 
+
+        System.out.println("---FUNCIONARIO ATUALIZADO---");
+        System.out.println(funcionario);
+
+        scanner.close();
+    //a entrada do usuario só funcionou quando rodei no terminal integrado, n sei se é normal :(
+
+
     }
 
-    Funcionario funcionario = new Funcionario(1001, "Thais", "11122233344", "Rua pipipi popopo", "83991151519", 23, 1200.12f);
-
-    System.out.println("")
 
 }
